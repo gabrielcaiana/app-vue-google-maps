d52f4158f49e0a0e2adfd5457d7a94734226dbcb
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-06-29 01:09:48 -0300

    refactor: errors

--- src/components/FormLocation.vue
+++ src/components/FormLocation.vue
@@ -2,11 +2,15 @@
   <section class="ui two column centered grid">
     <div class="column">
       <form class="ui segment large form">
-        <div class="ui message red" v-if="error"> {{ error }}</div>
+        <div class="ui message red" v-if="error">{{ error }}</div>
         <div class="ui segment">
           <div class="field">
             <div class="ui right icon input large">
-              <input v-model="address" type="text" placeholder="Enter your address" />
+              <input
+                v-model="address"
+                type="text"
+                placeholder="Enter your address"
+              />
               <i class="dot circle link icon" @click="locatorButtonPressed"></i>
             </div>
           </div>
@@ -23,9 +27,9 @@ import LocationApi from '@/services/api'
 const api = new LocationApi()
 
 export default {
-  data:() => ({
+  data: () => ({
     address: '',
-    error: ''
+    error: '',
   }),
   methods: {
     locatorButtonPressed() {
@@ -39,24 +43,24 @@ export default {
           },
 
           error => {
-            console.log(error.message)
+            this.error = error.message
           }
         )
       } else {
-        console.log('Your browser does not support geolocation API')
+        this.error = 'Your browser does not support geolocation API'
       }
     },
 
     async getAddressFrom(lat, long) {
       try {
         const response = await api.get(lat, long)
-        if(response.status === 200 && response.data.results.length > 0) {
+        if (response.status === 200 && response.data.results.length > 0) {
           this.address = response.data.results[0].formatted_address
         } else {
           this.error = response.data.error_message
         }
       } catch (err) {
-        console.log(err)
+        this.error = err
       }
     },
   },
