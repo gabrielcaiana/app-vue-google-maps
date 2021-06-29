9d5f308f8950de3d6bb209848c420406d9fd4886
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-06-28 21:01:04 -0300

    configurando o vuesax

---
+++ src/views/UserLocation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="center grid" id="user-location">
+     <vs-row>
+      <vs-col vs-type="flex" justify="center" align="center" w="12">
+        <FormLocation />
+      </vs-col>
+     </vs-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+      FormLocation:() => import('@/components/FormLocation')
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  #user-location {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
